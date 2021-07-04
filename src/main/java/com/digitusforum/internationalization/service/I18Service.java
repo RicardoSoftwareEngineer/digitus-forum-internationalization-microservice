d3f696a9ff4afe27b104fcb83349a4e5e6f2dd08
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-04 18:28:05 -0300

    dependecy injection added for a more decoupled environment

--- src/main/java/com/digitusforum/internationalization/service/I18Service.java
+++ src/main/java/com/digitusforum/internationalization/service/I18Service.java
@@ -1,6 +1,7 @@
 package com.digitusforum.internationalization.service;
 
-//import microservice.HistoryMicroservice;
+import java.util.Locale;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.http.HttpStatus;
@@ -9,29 +10,22 @@ import org.springframework.web.server.ResponseStatusException;
 
 import vo.InternationalizationVO;
 
-import java.util.Locale;
-
 @Service
 public class I18Service implements MessageSourceAware {
-    private MessageSource messageSource;
-    //private HistoryMicroservice historyMicroservice = new HistoryMicroservice();
+	private MessageSource messageSource;
 
-    @Override
-    public void setMessageSource(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
+	@Override
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
-    public InternationalizationVO getInternationalizedString(InternationalizationVO i18){
-    	try {
-    		String message = this.messageSource.getMessage(i18.getKey(),
-                    new Object[0],
-                    new Locale(i18.getLocale()));
-            i18.setMessage(message);
-    	}catch (Exception e) {
+	public InternationalizationVO getInternationalizedString(InternationalizationVO i18) {
+		try {
+			String message = this.messageSource.getMessage(i18.getKey(), new Object[0], new Locale(i18.getLocale()));
+			i18.setMessage(message);
+		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.valueOf(404), e.getMessage());
 		}
-        
-        //historyMicroservice.create(i18);
-        return i18;
-    }
+		return i18;
+	}
 }
